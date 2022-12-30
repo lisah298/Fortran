c6d3b350af6fb22563f42c95ce86972db621f7a6
Lisa Hetzel <mail@lhetzel.de>
2022-12-30 22:02:43 +0100

    added call of subroutine

--- EX7/function5/main.f90
+++ EX7/function5/main.f90
@@ -2,11 +2,19 @@ program matrixmultiplication
     use construct
     implicit none
     integer :: dim
+    real, dimension(:, :), allocatable :: A, B
 
     !User input
     print*, 'Please enter the dimensionality of your square matrices'
-    read(*,*) dim   
+    read(*,*) dim
 
-    
+    !allocate and fill matrices
+    allocate(A(dim, dim))
+    allocate(B(dim, dim))
+    call fill(dim, A, B)
+    print*, A(1,1), B(1,1) 
+    print*, A(2,2), B(2,2) 
+
+    !Ask user to select method
     
 end program matrixmultiplication
