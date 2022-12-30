e0478c4a6b5cb1117e45da2648d5f820871241e8
Lisa Hetzel <mail@lhetzel.de>
2022-12-30 22:02:25 +0100

    finished filling of matrices

--- EX7/function5/construct.f90
+++ EX7/function5/construct.f90
@@ -1,13 +1,25 @@
 module construct
     contains
-    subroutine fill(i,j,n,a_ij, b_ij)
+    subroutine fill(n, A, B)
         implicit none
         real :: pi = 3.14159265359
-        integer, intent(in) :: i,j,n
-        real, intent(out):: a_ij,b_ij
+        real :: a_ij, b_ij
+        integer, intent(in) :: n
+        integer :: i, j
+        real, dimension(n, n), intent(out) :: A, B
     
-        a_ij = COS(2*pi*i*j/n)
-        b_ij = SIN(2*pi*i*j/n)
+
+        do i = 1, n
+          do j=i, n
+            a_ij = COS((2*pi*i*j)/n)
+            b_ij = SIN((2*pi*i*j)/n)
+            A(i,j) = a_ij
+            B(i,j)= b_ij
+            !matrix is symmetric under swap of columns and lines
+            A(j,i)= a_ij
+            B(j,i)= b_ij
+          end do
+        end do
       end subroutine
 
    
