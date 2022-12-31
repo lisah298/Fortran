6f2be40fd57cf323b32e3ec9165d46a0360b8c0d
Lisa Hetzel <mail@lhetzel.de>
2022-12-31 11:04:03 +0100

    added switch

--- EX7/function5/main.f90
+++ EX7/function5/main.f90
@@ -8,16 +8,31 @@ program matrixmultiplication
     print*, 'Please enter the dimensionality of your square matrices'
     read(*,*) dim
 
+    !Ask user to select method
     print*, 'Which method would you like to use? 0: Dummy, 1: BLAS, 2: own, 3: intrinsic'
     read(*,*) method
     !allocate and fill matrices
     allocate(A(dim, dim))
     allocate(B(dim, dim))
     call fill(dim, A, B)
-    print*, A(1,1), B(1,1) 
-    print*, A(2,2), B(2,2) 
+    !print*, A(1,1), B(1,1) 
+    !print*, A(2,2), B(2,2) 
 
-    !Ask user to select method
+    !switch for executing selected method
+    select case (method)
+        case (0)
+            print*, 'Dummy'
+        case (1)
+            print*, 'BLAS'
+        case (2)
+            print*, 'own'
+        case (3)
+            print*, 'oFortran'
+        case default
+            print*, 'None'
+    end select
+    
+
     
     
 end program matrixmultiplication
