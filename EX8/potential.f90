d3211320268f1e8e4f6410b5043c728cf6d6e725
Lisa Hetzel <mail@lhetzel.de>
2023-01-01 20:58:27 +0100

    added derivatives

--- EX8/potential.f90
+++ EX8/potential.f90
@@ -1,11 +1,25 @@
 module potential
     implicit none
     contains
-    real function  LJ_potential(x)
+
+    !LJ potential
+    real function  V(x)
     implicit none
     real, intent(in) :: x
- 
-    !exemplary function
-    LJ_potential = 4*(x**(-12)-x**(-6))
-    end function  LJ_potential
+    V = 4*(1/x**12-1/x**6)
+    end function  V
+
+    !force
+    real function  f(x)
+    implicit none
+    real, intent(in) :: x
+    f = -24*((-2/x**(13))+(1/x**(7)))
+    end function  f
+
+    !derivative of force
+    real function  f_der(x)
+    implicit none
+    real, intent(in) :: x
+    f_der = -24*((26/x**(14))-(7/x**(8)))
+    end function  f_der
 end module potential
