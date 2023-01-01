32ec613b3e2893e270e849da44a9f8b08ac198f8
Lisa Hetzel <mail@lhetzel.de>
2023-01-01 18:14:07 +0100

    debugged first order euler

--- EX8/algorithms.f90
+++ EX8/algorithms.f90
@@ -1,14 +1,15 @@
 module algorithms
+    use potential
     implicit none
     contains
-    subroutine fo_euler(dt, x, v, m, x_next, v_next)
+    subroutine fo_euler(dt, x0, v0, m, x_next, v_next)
         implicit none
-        real, intent(in) :: dt, x, v, m
+        real, intent(in) :: dt, x0, v0, m
         real, intent(out) :: x_next, v_next
         real :: a
-        x_next = x + v*dt
-        a = m
-        v_next = v + a*dt
+        x_next = x0 + v0 * dt
+        a = LJ_potential(x0)/m
+        v_next = v0 + a * dt
 
     end subroutine
 
