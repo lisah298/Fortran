40334a4704b0b506b6854d5ef52adfea07be10d1
Lisa Hetzel <mail@lhetzel.de>
2023-01-04 22:06:32 +0100

    tried debugging, failed

--- EX8/diffusion/main.f90
+++ EX8/diffusion/main.f90
@@ -1,10 +1,10 @@
 program MDprogram
     implicit none
-    real :: t_start, t_finish, msd
+    real :: t_start, t_finish, msd, msd_av, sum_msd, t, t0, dt, D
     !integer :: method
     character(20):: filename
     character(6) :: SOL, site, index
-    character(8) :: junk, abs_step, time
+    character(12) :: junk, abs_step, time, time0
     !character(1):: lines_known
     integer :: step, mol, atom, molecules, sites, steps, i
     real, allocatable :: r0(:,:,:), v0(:,:,:), r(:,:,:), v(:,:,:)
@@ -14,7 +14,7 @@ program MDprogram
     !let user enter file name
     print*, 'Please enter the name of your .gro file containing the trajectory.'
     !read(*,*) filename
-    filename = 'spc_short_20_25.gro'
+    filename = 'spc_full.gro'
     print*, 'How many water molecules does your system contain?'
     !read(*,*) molecules
     molecules = 221
@@ -27,7 +27,7 @@ program MDprogram
     print*, 'How many steps does your trajectory have?'
     !read(*,*) steps
 
-    steps= 500
+    steps = 50000
     
     allocate(r0(1:molecules,1:sites,1:3))
     allocate(v0(1:molecules,1:sites,1:3))
@@ -40,27 +40,28 @@ program MDprogram
    
     call cpu_time(t_start)
    
-    read(2,*) 
+    read(2,*) junk, junk, junk, time0, junk, abs_step
     read(2,*)
     do mol = 1, molecules
         do atom = 1, sites
             read(2,*)  SOL, site, index, r0(mol, atom, 1), r0(mol, atom, 2)&
             , r0(mol, atom, 3), v0(mol, atom, 1), v0(mol, atom, 2), v0(mol, atom, 3)
-            !print*, r0(mol, atom, 1), r0(mol, atom, 2)&
-            !, r0(mol, atom, 3), v0(mol, atom, 1), v0(mol, atom, 2), v0(mol, atom, 3)
+            print*, r0(mol, atom, 1), r0(mol, atom, 2)&
+            , r0(mol, atom, 3), v0(mol, atom, 1), v0(mol, atom, 2), v0(mol, atom, 3)
         end do
-        !print*, SOL
+        print*, SOL
     end do
 
     !call exit()
     !iterate through steps
+    sum_msd = 0
     do step = 1, steps
         !skip lines with number of molecules and time stats
         read(2,*)
-        read(2,*) junk, junk, junk,  time, junk, abs_step
+        read(2,*) junk, junk, junk, time, junk, abs_step
         read(2,*)
         msd = 0
-        !print*, junk2, junk2, junk3, junk4
+        !print*, time
         !iterate through molecules of a single step
         do mol = 1, molecules
             !iterate
@@ -72,18 +73,33 @@ program MDprogram
                 
                 !iterate over coordinates for computing the msd
                 do i = 1, 3
-                    msd = msd + (r0(mol, atom, i)-r(mol, atom, i))**2
+                    msd = msd + ((r(mol, atom, i)-r0(mol, atom, i))**2)
                     !print*, 'r0', r0(mol, atom, i), 'r', r(mol, atom, i)
                     
                 end do
                 !print*,'msd', msd
             end do
            !print*, SOL
-           msd = msd/sites
+           !sum_msd = sum_msd + msd
         end do
-        write(3,*) abs_step, time, msd
+        msd = (msd/(sites*molecules))
+        sum_msd = sum_msd + msd
+        write(3,*) abs_step, time, msd, sum_msd
     end do
     
+    print*
+    read(time,*)  t
+    read(time0,*)  t0
+    dt = (t - t0)/steps
+
+    print*, 'Timestep=', dt
+    print*, 'Time t=', t
+    print*, 'Time t0=', t0
+    print*, 'Time=', time
+    print*, 'Time0=', time0
+    msd_av = (sum_msd * dt)/t
+    D = (msd_av/(6*t)) * 10**3
+    print*, 'D=', D
 
     !open file and write header line
     !open(1, file = 'MD.log',status='unknown')
@@ -92,7 +108,7 @@ program MDprogram
     
   
     call cpu_time(t_finish)
-    print '("Execution time = ",f6.5," seconds.")', t_finish-t_start
+    print*, 'Execution time = ', t_finish-t_start
    
     deallocate (r0)
     deallocate (v0)
