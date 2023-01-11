9ce5bdfe91bbfa2cae30e6e36dda7e303dd591a5
Lisa Hetzel <mail@lhetzel.de>
2023-01-11 11:50:40 +0100

    edited documentation

--- EX7/function5/evalmem.f90
+++ EX7/function5/evalmem.f90
@@ -4,25 +4,22 @@ contains
 
 subroutine system_mem_usage(mem_string)
     implicit none
-    !real, intent(out) :: mem
     character(len=80) :: line
     character(len=8)  :: junk
     character(len=5), intent(out) :: mem_string
   
-    !This is horrible but it works
-   
+    !define bash command
     line = trim("top  -o mem -l 1 | grep 'main'  > mem.txt")
     call execute_command_line (line)
 
-    !save line of top to file
+    !save line output to file
     open(1, file='mem.txt', status='old')
 
-    !extract memory
+    !extract memory from file
     read(1, *) junk, junk, junk, junk, junk, junk, junk , mem_string
 
     !remove file
     call execute_command_line ('rm mem.txt')
-    return
     end subroutine system_mem_usage
     
 end module evalMem
