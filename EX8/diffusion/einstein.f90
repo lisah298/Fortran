dcbf7ad6985d9c836c6d94860d1539cb6d8e96cb
Lisa Hetzel <mail@lhetzel.de>
2023-01-14 19:55:21 +0100

    fixed einstein

--- EX8/diffusion/einstein.f90
+++ EX8/diffusion/einstein.f90
@@ -7,71 +7,70 @@ module einstein
         integer, intent(in):: molecules, sites
         character(20), intent(in):: filename
         character(6) :: SOL, site, index
-        character(12) :: junk, abs_step, time, time0
+        character(12) :: junk, abs_step, time
         real, intent(out) :: D
-        real :: r0(1:molecules,1:sites,1:3), r(1:molecules,1:sites,1:3)
-        real :: msd, sum_msd, t, msd_av
-        integer :: step, mol, atom, i, k, step_a, step_e,steps
+        real :: msd, t
+        integer :: step,  atom, dt_i,Nts,i, k, step_a, step_e, steps, N
+        real, allocatable :: pos(:, :, :)
+        !real, allocatable :: MSDs(:)
+        
+
         
     open (2, file = filename, status = 'old')
     open (3, file = 'MSD.out', status = 'unknown')
-    write(3,*) 'step    ', 'Time [ps]    ', 'msd [nm^2/ps]'
+    open (4, file = 'pos.out', status = 'unknown')
+    write(3,*) '#dt   ', 'msd [nm^2/ps]'
    
     print*, 'Einstein chosen. Computation started.'
     
-    !read in initial positions    
-    read(2,*) junk, junk, junk, time0, junk, abs_step
-    read(2,*)
+   
     
-    step_a = INT(ta/dt)+1
+    step_a = INT(ta/dt)
     step_e = INT(te/dt)+1
     steps = step_e - step_a
+    print*, steps
+    allocate(pos(1:3, 1:sites*molecules, 1:steps))
+    !allocate(MSD(1:3))
 
-
-    do k = 1, step_a
-        do mol = 1, molecules
-            do atom = 1, sites
-                read(2,*)  SOL, site, index, r0(mol, atom, 1), r0(mol, atom, 2), r0(mol, atom, 3), junk, junk, junk
-            end do
-        end do
-        read(2,*)
+    do step = 1,  steps-1
         read(2,*) junk, junk, junk, time, junk, abs_step
         read(2,*)
+        !print*, step
+        do atom = 1, sites*molecules
+                read(2,*)  SOL, site, index, pos(1:3, atom, step), junk, junk, junk
+                write(4, *) abs_step, atom, pos(1 ,atom, step), pos(2,atom, step), pos(3, atom, step)
+        end do
+        read(2,*)
+        
+   
     end do
-
-    !start computing msd
-    sum_msd = 0
-    do step = 1, step_e-step_a
+  
+    Nts = steps
+    do dt_i = 1, Nts
+        !print*, dt_i
+        N = Nts - dt_i
         msd = 0
-        !iterate through molecules of a single step
-        do mol = 1, molecules
-            !iterate over atoms/sites ins molecule
-            do atom = 1, sites
-                read(2,*)  SOL, site, index, r(mol, atom, 1), r(mol, atom, 2)&
-                , r(mol, atom, 3), junk, junk, junk
-                
-                !iterate over coordinates for computing the msd
+        !iterate over all possible combinations for dt=dt_i
+        do k = 1 , Nts - dt_i
+            !iterate over all atoms for one dt
+            do atom = 1, sites*molecules
                 do i = 1, 3
-                    msd = msd + ((r(mol, atom, i)-r0(mol, atom, i))**2)
-                    
+                    msd = msd + (pos(i, atom, k) - pos(i, atom, k+dt_i))**2
                 end do
             end do
         end do
-        msd = (msd/(sites*molecules))
-        sum_msd = sum_msd + msd
-        write(3,*) abs_step, time, msd
-        !skip lines with number of molecules and time stats
-        read(2,*)
-        read(2,*) junk, junk, junk, time, junk, abs_step
-        read(2,*)
+        msd = msd/(sites*molecules)
+        msd = msd /(Nts - dt_i)
+        t = dt_i * dt
+        write(3,*) dt_i, t, msd 
     end do
-    
+   
     !compute D
-    t = te - ta
-    msd_av = (sum_msd)/steps
-    D = ((2*msd_av)/(6*t*10**2))
-    print*, 'D =', D
-
+    !t = te - ta
+    !msd_av = (sum_msd)/steps
+    !D = sum_msd/(6*t)
+    !print*, 'D =', D
+    D=3
   
     
     end subroutine
