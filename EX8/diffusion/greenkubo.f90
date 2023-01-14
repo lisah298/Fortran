d1e06ac8eee371709f1e44d8c7009e61a57151a9
Lisa Hetzel <mail@lhetzel.de>
2023-01-14 21:36:35 +0100

    fiexed Green-Kubo

--- EX8/diffusion/greenkubo.f90
+++ EX8/diffusion/greenkubo.f90
@@ -7,68 +7,62 @@ module greenkubo
         integer, intent(in):: molecules, sites
         character(20), intent(in):: filename
         character(6) :: SOL, site, index
-        character(12) :: junk, abs_step, time, time0
+        character(12) :: junk, abs_step, time
         real, intent(out) :: D
-        real :: v0(1:molecules,1:sites,1:3), v(1:molecules,1:sites,1:3)
-        real :: v_square, sum_vsquare, t
-        integer :: step, mol, atom, i, k, step_a, step_e,steps
+        real :: v_square, riemann_sum, t
+        integer :: step,  atom, dt_i,Nts, i, k, step_a, step_e, steps, N
+        real, allocatable :: vel(:, :, :)
         
-    open (2, file = filename, status = 'old')
+    
    
    
     print*, 'Green-Kubo chosen. Computation started.'
-    
-    !read in initial positions    
-    read(2,*) junk, junk, junk, time0, junk, abs_step
-    read(2,*)
-    
-    !Compute first end last step based on user in
-    step_a = INT(ta/dt)+1
+
+    open (2, file = filename, status = 'old')
+    open (3, file = 'vel_auto.out', status = 'unknown')
+    open (4, file = 'vel.out', status = 'unknown')
+    write(3,*) '#dt       t       v^2 [nm^2/ps^2]       integral'
+   
+    step_a = INT(ta/dt)
     step_e = INT(te/dt)+1
     steps = step_e - step_a
+    print*, steps
+    allocate(vel(1:3, 1:sites*molecules, 1:steps))
+   
 
-    open (3, file = 'v_autokorr.out', status = 'unknown')
-    write(3,*) 'step    ', 'Time [ps]    ', 'v(0)*v(t) [nm^2/ps^2]'
-
-    do k = 1, step_a
-        do mol = 1, molecules
-            do atom = 1, sites
-                read(2,*)  SOL, site, index, junk, junk, junk, v0(mol, atom, 1), v0(mol, atom, 2), v0(mol, atom, 3)
-            end do
-        end do
-        read(2,*)
+    do step = 1,  steps+1
         read(2,*) junk, junk, junk, time, junk, abs_step
         read(2,*)
+        do atom = 1, sites*molecules
+                read(2,*)  SOL, site, index, junk, junk, junk, vel(1:3, atom, step)
+                write(4, *) abs_step, atom, vel(1 ,atom, step), vel(2,atom, step), vel(3, atom, step)
+        end do
+        read(2,*)
     end do
-
-    !start computing msd
-    sum_vsquare = 0
-    do step = 1, step_e-step_a
+  
+    riemann_sum = 0
+    Nts = steps+1
+    do dt_i = 1, Nts
+        !print*, dt_i
+        N = Nts - dt_i
         v_square = 0
-        !iterate through molecules of a single step
-        do mol = 1, molecules
-            !iterate over atoms/sites ins molecule
-            do atom = 1, sites
-                read(2,*)  SOL, site, index, junk, junk, junk, v(mol, atom, 1), v(mol, atom, 2)&
-                , v(mol, atom, 3)
-                !iterate over coordinates for computing the msd
+        !iterate over all possible combinations for dt=dt_i
+        do k = 1 , Nts - dt_i
+            !iterate over all atoms for one dt
+            do atom = 1, sites*molecules
                 do i = 1, 3
-                    v_square = v_square + ((v(mol, atom, i)*v0(mol, atom, i)))/((v0(mol, atom, i)*v0(mol, atom, i)))
+                    v_square = v_square + (vel(i, atom, k) * vel(i, atom, k+dt_i))
                 end do
             end do
         end do
-        v_square = (v_square/(sites*molecules))
-        sum_vsquare = sum_vsquare + dt * v_square
-        write(3,*) abs_step, time, v_square
-        !skip lines with number of molecules and time stats
-        read(2,*)
-        read(2,*) junk, junk, junk, time, junk, abs_step
-        read(2,*)
+        v_square = v_square/(sites*molecules)
+        v_square = v_square /(Nts - dt_i)
+        t = dt_i * dt
+        riemann_sum = riemann_sum + v_square * dt
+        write(3,*) dt_i, t, v_square, riemann_sum 
     end do
     
-    !compute D
-    t = te - ta
-    D = ((sum_vsquare)/(3.0*t)*10**(-2.0))
+    D = ((riemann_sum)/(3.0)*10**(-2.0))
     print*, 'D [cm^2/s] =', D
 
   
