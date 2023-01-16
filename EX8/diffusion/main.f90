8bd324ae6dab4309ea42929a02948429cd61ea45
Lisa Hetzel <mail@lhetzel.de>
2023-01-16 10:52:27 +0100

    added documentation

--- EX8/diffusion/main.f90
+++ EX8/diffusion/main.f90
@@ -6,26 +6,20 @@ program diffusion
     integer :: method
     character(20):: filename
     integer :: molecules, sites
-    real, allocatable :: r0(:,:,:), v0(:,:,:), r(:,:,:), v(:,:,:)
-    !real, allocatable :: MSD(:)
 
-
-    !let user enter file name
+    !let user enter information
     print*, 'Please enter the name of your .gro file containing the trajectory.'
-    !read(*,*) filename
-    filename = 'spc_full_no_pbc.gro'
+    read(*,*) filename
+   
     print*, 'How many water molecules does your system contain?'
-    !read(*,*) molecules
     molecules = 221
 
     print*, 'How many sites/atoms does the molecules of your system contain?'
-    !read(*,*) sites
     sites = 3
 
     !ask user about size of file
     print*, 'What is the timestep of your trajectory?'
-    !read(*,*) dt
-    dt = 0.002
+    read(*,*) dt
 
     print*, 'Pleas enter start and end time for computation as ta te'
     read(*,*) ta, te
@@ -34,32 +28,15 @@ program diffusion
     print*, 'Which method do you want to use for computing the diffusion coefficient? 1: Einstein, 2: Green-Kubo'
     read(*,*) method
     
+    call cpu_time(t_start)
     select case (method)
     case (1)
-        allocate(r0(1:molecules,1:sites,1:3))
-        allocate(r(1:molecules,1:sites,1:3))
-        call cpu_time(t_start)
         call diffusion_einstein(filename, molecules, sites, ta, te, dt, D)
-        call cpu_time(t_finish)
-        deallocate (r0)
-        deallocate (r)
     case (2)
-        allocate(v0(1:molecules,1:sites,1:3))
-        allocate(v(1:molecules,1:sites,1:3))
-        call cpu_time(t_start)
         call diffusion_greenkubo(filename, molecules, sites, ta, te, dt, D)
-        call cpu_time(t_finish)
-        deallocate (v0)
-        deallocate (v)
     end select
+    
+    call cpu_time(t_finish)
+    print*, 'Computation is done. Execution time = ', t_finish-t_start
    
-   
-    
-    
-  
-    
-    print*, 'Execution time = ', t_finish-t_start
-   
-    
-    
 end program diffusion
