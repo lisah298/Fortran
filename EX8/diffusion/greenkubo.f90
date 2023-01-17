098f195447971acd06eaba3dec579b8e6d7f0a7a
Lisa Hetzel <mail@lhetzel.de>
2023-01-17 18:46:15 +0100

    added documentation

--- EX8/diffusion/greenkubo.f90
+++ EX8/diffusion/greenkubo.f90
@@ -1,16 +1,16 @@
 module greenkubo
     implicit none
     contains
-    subroutine diffusion_greenkubo(filename, molecules, sites, ta, te, dt, D)
+    subroutine diffusion_greenkubo(filename, molecules, sites, t_ges, dt, D)
         implicit none
-        real, intent(in) :: ta, te, dt
+        real, intent(in) :: dt, t_ges
         integer, intent(in):: molecules, sites
         character(20), intent(in):: filename
         character(6) :: SOL, site, index
         character(12) :: junk, abs_step, time
         real, intent(out) :: D
         real :: v_square, riemann_sum, t
-        integer :: step,  atom, dt_i,Nts, i, k, step_a, step_e, steps, N
+        integer :: step,  atom, dt_i,Nts, i, k, steps, N
         real, allocatable :: vel(:, :, :)
         
     
@@ -23,13 +23,12 @@ module greenkubo
     open (4, file = 'vel.out', status = 'unknown')
     write(3,*) '#dt       t       v^2 [nm^2/ps^2]       integral'
    
-    step_a = INT(ta/dt)
-    step_e = INT(te/dt)+1
-    steps = step_e - step_a
+    !compute total number of steps
+    steps = INT(t_ges/dt)
     print*, steps
     allocate(vel(1:3, 1:sites*molecules, 1:steps))
    
-
+    !read in velocities and write into vel.out file
     do step = 1,  steps+1
         read(2,*) junk, junk, junk, time, junk, abs_step
         read(2,*)
@@ -43,7 +42,6 @@ module greenkubo
     riemann_sum = 0
     Nts = steps+1
     do dt_i = 1, Nts
-        !print*, dt_i
         N = Nts - dt_i
         v_square = 0
         !iterate over all possible combinations for dt=dt_i
@@ -64,8 +62,6 @@ module greenkubo
     
     D = ((riemann_sum)/(3.0)*10**(-2.0))
     print*, 'D [cm^2/s] =', D
-
-  
     
     end subroutine
 
